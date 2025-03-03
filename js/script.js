72f6bde2ee3a096caf460e04766404f2118a622c
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2025-03-03 16:38:54 +0600

    nav fixed

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSTAGn5lgADakvtIZZ6eKXFSlQxv89peo",
-  authDomain: "taleemul-quran.firebaseapp.com",
-  projectId: "taleemul-quran",
-  storageBucket: "taleemul-quran.firebasestorage.app",
-  messagingSenderId: "152516080082",
-  appId: "1:152516080082:web:129246bfd9d897b59c180b",
-  measurementId: "G-C7K7MFYKBJ"
+    apiKey: "AIzaSyBSTAGn5lgADakvtIZZ6eKXFSlQxv89peo",
+    authDomain: "taleemul-quran.firebaseapp.com",
+    projectId: "taleemul-quran",
+    storageBucket: "taleemul-quran.firebasestorage.app",
+    messagingSenderId: "152516080082",
+    appId: "1:152516080082:web:129246bfd9d897b59c180b",
+    measurementId: "G-C7K7MFYKBJ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+alert('hi')
